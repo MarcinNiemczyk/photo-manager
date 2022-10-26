066a26961eeff199b2e101b4a2c05e88d7bd8a40
martindustry <marcin.exn@gmail.com>
2022-10-26 16:34:46 +0200

    Change serializer fields

--- api/serializers.py
+++ api/serializers.py
@@ -4,15 +4,13 @@ from .models import Photo
 
 class PhotoSerializer(serializers.ModelSerializer):
 
-    url = serializers.SerializerMethodField('get_image_url')
+    image = serializers.SerializerMethodField('get_image_url')
+    url = serializers.URLField(write_only=True, label='URL')
 
     class Meta:
         model = Photo
         fields = ('id', 'title', 'album_id', 'width', 'height', 'color', 'image', 'url')
-        read_only_fields = ('width', 'height', 'color', 'url')
-        extra_kwargs = {
-            'image': {'write_only': True}
-        }
+        read_only_fields = ('width', 'height', 'color', 'image')
 
     def get_image_url(self, obj):
         return obj.image.url
